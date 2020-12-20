64c5a2a83bfe5ffa1d28700c054a6b39d086f686
Jordan Chadwick <jordan.chadwick@gmail.com>
2020-12-20 16:07:56 -0500

    checks for effective UID 0 in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,10 @@
 # bootstrap.sh
 #
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 apt update && apt install ansible
 ansible-playbook --syntax-check local.yml && ansible-playbook local.yml
