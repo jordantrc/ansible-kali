f5b6afaf930f971d1102a03ab9a017ee6b42f28a
Jordan Chadwick <jordan.chadwick@gmail.com>
2020-12-20 15:52:07 -0500

    fixes rdpscan install script

--- roles/common/scripts/rdpscan.sh
+++ roles/common/scripts/rdpscan.sh
@@ -3,7 +3,5 @@
 unzip /tmp/rdpscan.zip -d /tmp/rdpscan
 cd /tmp/rdpscan
 make
-mv rdpscan /usr/local/bin/rdpscan
+mv /tmp/rdpscan /usr/local/bin/rdpscan
 chmod +x /usr/local/bin/rdpscan
-cd /tmp
-rm -rf /tmp/rdpscan
